88c1637cf63964e9d9004ad1102678bc90efd785
SayantanStackflow <sayantan.college6291@gmail.com>
2023-02-27 18:40:58 +0000

    animation time frame change

--- Utilities/motions.js
+++ Utilities/motions.js
@@ -14,7 +14,7 @@ export const navVariants = {
     transition: {
       type: 'spring',
       stiffness: 80,
-      delay: 1.3,
+      delay: 1,
     },
   },
 };
--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
           href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Caveat&family=Fjalla+One&family=Oswald&display=swap"
           rel="stylesheet"
