95fe7fa8ab7dedd51c6b9cc3337fcbf632e67704
SayantanStackflow <sayantan.college6291@gmail.com>
2023-03-01 22:33:52 +0000

    added motions to tutorial.jsx

--- components/tutorial.jsx
+++ components/tutorial.jsx
@@ -2,25 +2,36 @@ import Image from 'next/image';
 import SpamExp from '../public/example_spam.png';
 import { HiChevronRight } from 'react-icons/hi';
 import { HiChevronDoubleRight } from 'react-icons/hi';
+import { staggerContainer, fadeIn, ImageVarients, zoomIn } from '@/Utilities/motions';
+import { motion } from 'framer-motion';
 
 const tutorial = () => {
   return (
     <div className="flex  justify-center py-24">
-      <div className="flex max-w-[1240px] items-center lg:flex-row flex-col-reverse lg:gap-28 gap-8 overflow-hidden">
-        <Image
-          src={SpamExp}
-          alt="spamcomments"
-          width={450}
-          className="object-contain lg:w-[50%] md:w-[70%] w-[90%] h-auto flex justify-center"
-        />
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: 'false ', amount: 0.25 }}
+        className="flex max-w-[1240px] items-center lg:flex-row flex-col-reverse lg:gap-28 gap-8 overflow-hidden"
+      >
+        <motion.div variants={fadeIn('left', 'tween', 0.2, 1)} className="flex justify-center">
+          <Image
+            src={SpamExp}
+            alt="spamcomments"
+            width={450}
+            className="object-contain lg:w-[100%] md:w-[70%] w-[100%] h-auto"
+          />
+        </motion.div>
         <div className="lg:max-w-[985px] flex flex-col gap-5">
-          <h1
+          <motion.h1
+            variants={fadeIn('up', 'tween', 0.2, 0.8)}
             style={{ fontFamily: 'Fjalla One, sans-serif' }}
             className=" max-w-[700px] capitalize lg:text-[54px] lg:leading-[68px] leading-[50px] md:text-[45px] md:font-medium text-[40px] font-medium text-[#2D7D90] "
           >
             Verify the spam comments with a few easy steps
-          </h1>
-          <div className="flex justify-start">
+          </motion.h1>
+          <motion.div variants={fadeIn('up', 'tween', 0.3, 1)} className="flex justify-start">
             <button className="border-2 border-[#0D4C92] px-3 py-[6px]  rounded-full flex items-center justify-center flex-row hover:bg-[#0D4C92] duration-500 hover:text-white text-[#0D4C92] hover:gap-2 ">
               <a
                 style={{ fontFamily: 'Sarabun, sans-serif' }}
@@ -30,8 +41,8 @@ const tutorial = () => {
               </a>
               <HiChevronRight size={18} />
             </button>
-          </div>
-          <div className="flex justify-start">
+          </motion.div>
+          <motion.div variants={fadeIn('up', 'tween', 0.4, 1)} className="flex justify-start">
             <button className=" px-4 py-2  rounded-full flex items-center justify-center flex-row bg-[#1d5567] hover:bg-[#003049] duration-500 text-white hover:gap-2 ">
               <a
                 style={{ fontFamily: 'Sarabun, sans-serif' }}
@@ -41,9 +52,9 @@ const tutorial = () => {
               </a>
               <HiChevronDoubleRight size={18} />
             </button>
-          </div>
+          </motion.div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
