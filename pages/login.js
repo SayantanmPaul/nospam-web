72e9b93637e5c5229c2a6721700704f0d7f44388
SayantanStackflow <sayantan.college6291@gmail.com>
2023-03-02 15:32:39 +0000

    firsrt commit login/regisrer page

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const Login = () => {
+  return (
+    <div>
+      <Head>
+        <title>Login: NoSpam.</title>
+      </Head>
+      <h1 className="font-bold text-4xl text-blue-700">This is Login Page</h1>
+    </div>
+  );
+};
+
+export default Login;
---
+++ pages/register.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+const Register = () => {
+  return (
+    <div>
+      <Head>
+        <title>Register: NoSpam.</title>
+      </Head>
+      <h1 className="font-bold text-4xl text-green-700">This is Register Page</h1>
+    </div>
+  );
+};
+
+export default Register;
