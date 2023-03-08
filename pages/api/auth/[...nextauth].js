2d2561255fd516ba232bfca7758c0ecdf9ed4972
SayantanStackflow <sayantan.college6291@gmail.com>
2023-03-08 08:16:45 +0000

    currently not using mongodb credential

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,6 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import GithubProvider from 'next-auth/providers/github';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import connectMongo from '@/database/credentials';
-import { connect } from 'mongoose';
-import Users from '@/model/schema';
-import { compare } from 'bcryptjs';
 
 export default NextAuth({
   providers: [
@@ -19,29 +14,6 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-    }),
-
-    CredentialsProvider({
-      name: 'credentials',
-      async authorize(credentials, req) {
-        connectMongo().catch((error) => {
-          error: 'Connection Failed!';
-        });
-        //check user exists
-        const result = await Users.findOne({ email: credentials.email });
-        if (!result) {
-          throw new Error('No user found with this email !');
-        }
-
-        //compare password
-        const checkPassword = await compare(credentials.password, result.password);
-
-        //incorrect password
-        if (!checkPassword || result.email !== credentials.email) {
-          throw new error("Username or Password didn't match!");
-        }
-        return result;
-      },
     }),
   ],
   secret: process.env.JWT_SECRET,
